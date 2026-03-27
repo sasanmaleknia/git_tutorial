389a53c87263967175959ddbb34f7d22f0f33882
Sasan Maleknia <malekniasasan01@gmail.com>
2026-03-27 12:46:10 +0100

    sina commit

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ def main():
     print("Hello World! ")
     print("I am fixing the issue #1")
 
+def fun(name):
+    print(f"hello dear {name}, im here to fix your issues. ")
+
 if __name__ == "__main__":
     main()
+    fun("sina")
